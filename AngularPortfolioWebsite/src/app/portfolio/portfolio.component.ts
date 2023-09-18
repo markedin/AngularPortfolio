39124a7ce9b2b06b6e108dbca18340db02c6673a
markedin <119455170+markedin@users.noreply.github.com>
2023-09-18 15:06:26 -0700

    Adding the ability to pass data to components.
    Adding models and the ability to pass data to the project card components, such as name, description, summary, tags and tag color etc

--- AngularPortfolioWebsite/src/app/portfolio/portfolio.component.ts
+++ AngularPortfolioWebsite/src/app/portfolio/portfolio.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Project } from '../_models/Project';
+import { Tag } from '../_models/Tag';
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent {
+
+  project: Project = {
+    id: 0,
+    name: 'Sample angular app',
+    summary: 'Test description',
+    description: '',
+    projectLink: '',
+    tags: [Tag.ANGULAR, Tag.TYPESCRIPT],
+    pictures: []
+    
+
+  };
 
   constructor(private titleService: Title) {
     this.titleService.setTitle('Mark Edin - Portfolio');
