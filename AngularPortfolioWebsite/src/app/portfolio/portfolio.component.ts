7024acdf4db6b9725d4f3527a044b842a8e7e942
markedin <119455170+markedin@users.noreply.github.com>
2023-09-20 14:49:51 -0700

    Adding project service
    This push includes adding the project service as well as the ability to pass data through the featured project tile on the home component

--- AngularPortfolioWebsite/src/app/portfolio/portfolio.component.ts
+++ AngularPortfolioWebsite/src/app/portfolio/portfolio.component.ts
@@ -1,28 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Project } from '../_models/Project';
 import { Tag } from '../_models/Tag';
+import { ProjectsService } from '../_services/projects.service';
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.css']
 })
-export class PortfolioComponent {
+export class PortfolioComponent implements OnInit {
 
-  project: Project = {
-    id: 0,
-    name: 'Sample angular app',
-    summary: 'Test description',
-    description: '',
-    projectLink: '',
-    tags: [Tag.ANGULAR, Tag.TYPESCRIPT],
-    pictures: []
-    
+  projects = {} as Project[];
 
-  };
-
-  constructor(private titleService: Title) {
+  constructor(private titleService: Title, private projectService: ProjectsService) {
     this.titleService.setTitle('Mark Edin - Portfolio');
+  }
+  ngOnInit(): void {
+    this.projects = this.projectService.GetProjects();
   }
 
 }
